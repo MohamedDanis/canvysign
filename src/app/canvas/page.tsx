3ca198c6fc8a5b0a893da6be0b41d7f476d34a13
Mohamed Danis <62685219+MohamedDanis@users.noreply.github.com>
2023-07-27 12:07:52 +0530

    added env

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -85,7 +85,8 @@ function Page() {
         getImages()
 
     }, [])
-
+    console.log(process.env.AWS_ACCESS_KEY_ID,34);
+    
 
     return (
         <>
@@ -147,59 +148,6 @@ function Page() {
 
 
             {/* modal */}
-            {/* <Transition.Root show={open} as={Fragment}>
-                <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={handleOpen}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 z-10 overflow-y-auto">
-                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            >
-                                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white h-[600px] text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                        <div className="sm:flex sm:items-start md:justify-center px-12">
-
-                                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                                <Dialog.Title as="h1" className="text-xl font-semibold leading-6 text-gray-900">
-                                                    Add new Signature
-                                                </Dialog.Title>
-                                                <div className="mt-2 px-8">
-                                                    <form action="" className='flex flex-col'>
-                                                        <label htmlFor="">Name</label>
-                                                        <input type="text" className='border-2 border-neutral-800 rounded-md py-2 px-4' />
-                                                        <label htmlFor="">Date</label>
-                                                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className='border-2 border-neutral-800 rounded-md py-2 px-4' />
-                                                        <label htmlFor="">Signature</label>
-                                                        <DynamicDrawingCanvas/>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition.Root> */}
             <Transition appear show={isOpen} as={Fragment}>
                 <Dialog as="div" className="relative z-10" onClose={closeModal}>
                     <Transition.Child
@@ -233,14 +181,14 @@ function Page() {
                                         Add New Signature
                                     </Dialog.Title>
                                     <div className="mt-2">
-                                        <form action="" className='flex flex-col'>
+                                        <div className='flex flex-col'>
                                             <label htmlFor="">Name</label>
                                             <input type="text" className='border-2 border-neutral-800 rounded-md py-2 px-4' />
                                             <label htmlFor="">Date</label>
                                             <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className='border-2 border-neutral-800 rounded-md py-2 px-4' />
                                             <label htmlFor="">Signature</label>
                                             <DynamicDrawingCanvas ref={childRef}/>
-                                        </form>
+                                        </div>
                                     </div>
 
                                     <div className="mt-4">
--- src/components/DrawingCanvas/DrawingCanvas.tsx
+++ src/components/DrawingCanvas/DrawingCanvas.tsx
@@ -2,19 +2,21 @@ import React, { useState, useRef, forwardRef } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
 import Konva from 'konva'
 import s3 from '@/utils/s3'
+import DatePicker from "react-datepicker";
 
-const DrawingCanvas = React.forwardRef((props,ref) => {
+const DrawingCanvas = React.forwardRef((props, ref) => {
   const [lines, setLines] = useState<any>([]);
   const [isDrawing, setIsDrawing] = useState<any>(false);
   const stageRef = useRef<Konva.Stage>(null);
+  const [startDate, setStartDate] = useState<any>(new Date());
 
-  const handleMouseDown = (e:any) => {
+  const handleMouseDown = (e: any) => {
     setIsDrawing(true);
     const { offsetX, offsetY } = e.evt;
     setLines([...lines, { points: [offsetX, offsetY] }]);
   };
 
-  const handleMouseMove = (e:any) => {
+  const handleMouseMove = (e: any) => {
     if (!isDrawing) return;
     const { offsetX, offsetY } = e.evt;
     const lastLine = lines[lines.length - 1];
@@ -26,39 +28,46 @@ const DrawingCanvas = React.forwardRef((props,ref) => {
     setIsDrawing(false);
   };
 
- 
-    const handleSave = async() => {
-        if (!stageRef.current) return; // add null check
-        const uri = stageRef.current.toDataURL();
-        const byteString = atob(uri.split(',')[1]);
-        const ab = new ArrayBuffer(byteString.length);
-        const ia = new Uint8Array(ab);
-        for (let i = 0; i < byteString.length; i++) {
-          ia[i] = byteString.charCodeAt(i);
-        }
-        const blob = new Blob([ab], { type: 'image/png' });
-        const file = new File([blob], 'image.png', { type: 'image/png' });
-        console.log(file);
-        // to s3 upload
-        const min = 1;
-        const max = 100;
-        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        try {
-          const params = {
-            Bucket: 'canvisign',
-            Key: `signd${randomNum}`,
-            Body: file,
-          };
-          await s3.upload(params).promise();
-          console.log('File uploaded successfully!');
-        } catch (err) {
-          console.error('Error uploading file:', err);
-        }
-        
-      }
-    
+
+  const handleSave = async () => {
+    if (!stageRef.current) return; // add null check
+    const uri = stageRef.current.toDataURL();
+    const byteString = atob(uri.split(',')[1]);
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([ab], { type: 'image/png' });
+    const file = new File([blob], 'image.png', { type: 'image/png' });
+    console.log(file);
+    // to s3 upload
+    const min = 1;
+    const max = 100;
+    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    try {
+      const params = {
+        Bucket: 'canvisign',
+        Key: `signd${randomNum}`,
+        Body: file,
+      };
+      await s3.upload(params).promise();
+      console.log('File uploaded successfully!');
+    } catch (err) {
+      console.error('Error uploading file:', err);
+    }
+
+  }
+
   return (
-    <div>
+    <div className='flex flex-col'>
+      
+        {/* <label htmlFor="">Name</label>
+        <input type="text" className='border-2 border-neutral-800 rounded-md py-2 px-4' />
+        <label htmlFor="">Date</label>
+        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className='border-2 border-neutral-800 rounded-md py-2 px-4' />
+        <label htmlFor="">Signature</label> */}
+     
       <Stage
         ref={stageRef}
         width={300}
@@ -69,16 +78,18 @@ const DrawingCanvas = React.forwardRef((props,ref) => {
         className='border-2 border-neutral-800'
       >
         <Layer>
-          {lines.map((line:any, i:any) => (
+          {lines.map((line: any, i: any) => (
             <Line key={i} points={line.points} stroke="black" strokeWidth={5} />
           ))}
         </Layer>
       </Stage>
+
       <button onClick={handleSave}
         className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500  mt-4"
       >Save Drawing</button>
+      
     </div>
   );
-  });
+});
 
 export default DrawingCanvas;
